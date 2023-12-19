b57b3ad1f814c6434e21fc5c7bd3670bccebbb7c
zhansvltan <assemzhar@icloud.com>
2023-12-19 22:28:19 +0600

    Image section mobile version

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,34 +5,41 @@ export default function ImageComponent() {
       <img
         src="card1.svg"
         alt="background"
-        style={{ width: '100%', height: '560px', objectFit: 'cover' }}
-        className="rounded-none brightness-50 pt-2"
+        className="rounded-none brightness-50 pt-2 w-[100%] object-cover
+                    mobile:h-[400px]
+                    tablet:h-[500px]
+                    laptop:h-[560px]"
       />
       <div
-        style={{ left: '55px', top: '170px' }}
-        className="absolute flex-col justify-between space-y-10"
+        className="absolute flex-col justify-between space-y-10 
+                    mobile:left-[30px] mobile:top-[120px]
+                    tablet:left-[45px] tablet:top-[150px]
+                    laptop:left-[55px] laptop:top-[170px]"
       >
         <h2
-          style={{
-            maxWidth: '646px',
-            width: '100%',
-          }}
-          className="text-white text-[52px] font-[500]"
+          className="text-white font-[500] max-w-[646px]
+                      mobile:text-[24px] mobile:w-[40%] 
+                      tablet:text-[36px] tablet:w-[60%] 
+                      laptop:text-[52px] laptop:w-[100%]"
         >
           Формируем будущее инфраструктуры
         </h2>
         <p
-          style={{
-            maxWidth: '610px',
-            width: '100%',
-          }}
-          className="text-[24px] text-white font-[100]"
+          className="text-white font-[100] max-w-[610px]
+                    mobile:text-[12px] mobile:w-[40%] 
+                    tablet:text-[18px] tablet:w-[60%] 
+                    laptop:text-[24px] laptop:w-[100%]"
         >
           Частная консалтинговая компания, работающая в формате фабрики мысли,
           расположенная в Астане в юрисдикции МФЦА
         </p>
-        <button className="leading-tight text-[16px] text-[#000000] bg-[#c3f2cb] rounded-3xl font-[500] w-[316px] h-[45px]">
-          Узнать подробнее
+        <button
+          className="leading-tight text-[#000000] bg-[#c3f2cb] rounded-3xl font-[500] 
+                      mobile:w-[105px] mobile:h-[25px] mobile:text-[8px]
+                      tablet:w-[210px] tablet:h-[40px] tablet:text-[12px]
+                      laptop:w-[316px] laptop:h-[45px] laptop:text-[16px]"
+        >
+          Оставить заявку
         </button>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,26 +16,40 @@ module.exports = {
   ],
   theme: {
     fontSize: {
-      '2xl': ['1.5rem', {
-        lineHeight: '2rem',
-        letterSpacing: '-0.01em',
-        fontWeight: '500',
-      }],
-      '2.5xl': ['1.625rem', {
-        lineHeight: '2.125rem',
-        letterSpacing: '-0.015em',
-        fontWeight: '400',
-      }
-
+      '2xl': [
+        '1.5rem',
+        {
+          lineHeight: '2rem',
+          letterSpacing: '-0.01em',
+          fontWeight: '500',
+        },
       ],
-      '3xl': ['1.875rem', {
-        lineHeight: '2.25rem',
-        letterSpacing: '-0.02em',
-        fontWeight: '700',
-      }],
+      '2.5xl': [
+        '1.625rem',
+        {
+          lineHeight: '2.125rem',
+          letterSpacing: '-0.015em',
+          fontWeight: '400',
+        },
+      ],
+      '3xl': [
+        '1.875rem',
+        {
+          lineHeight: '2.25rem',
+          letterSpacing: '-0.02em',
+          fontWeight: '700',
+        },
+      ],
+    },
+    screens: {
+      mobile: '320px',
+      // mobile
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... }
+      laptop: '1280px',
+      // => @media (min-width: 1024px) { ... }
     },
     extend: {},
   },
   plugins: [],
 }
-
