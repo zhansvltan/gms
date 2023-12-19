4f4d6d2da1627c5e661279590e1829d2c477f695
zhansvltan <assemzhar@icloud.com>
2023-12-20 01:00:27 +0600

    Project section mobile adaptation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,23 @@ const Card = ({ imageSrc, name, description, logo }) => {
   return (
     <div
       className="rounded-[24px] overflow-hidden shadow-[0px_2px_10px_rgba(3,3,3,0.1)]
-      max-h-[481px]
+      h-[481px]
       mobile:h-[60%]
-      tablet:h-[75%] 
-      laptop: h-[100%]"
+      tablet:h-[150%] tablet:ml-[50px] tablet:mr-[50px]
+      laptop:h-[100%]"
     >
       <img
-        className="w-full mobile:pb-[15px] tablet:pb-[20px] laptop:pb-[25px]"
+        className="w-full 
+        mobile:pb-[15px] 
+        tablet:pb-[20px] 
+        laptop:pb-[25px]"
         src={imageSrc}
         alt={name}
       />
       <div className="mobile:pl-[15px] tablet:pl-[20px] laptop:pl-[25px]">
         <h3
-          className="text-[#030303] mobile:text-[16px] mobile:pb-[12px] 
+          className="text-[#030303] 
+        mobile:text-[16px] mobile:pb-[12px] 
         tablet:text-[20px] tablet:pb-[16px] 
         laptop:text-[24px] laptop: pb-[20px]"
         >
@@ -25,7 +29,7 @@ const Card = ({ imageSrc, name, description, logo }) => {
         <p
           className="text-[#030303] 
         mobile:h-[96px]  mobile:text-[12px] 
-        tablet:h-[120px] tablet:text-[16px] 
+        tablet:h-[144px] tablet:text-[16px] 
         laptop:h-[144px] laptop:text-[20px]"
         >
           {description}
@@ -34,7 +38,7 @@ const Card = ({ imageSrc, name, description, logo }) => {
           className="text-[#030303]  
         mobile:text-[12px] mobile:pb-[12px] 
         tablet:text-[16px] tablet:pb-[16px]  
-        laptop:text-[20px] laptop: pb-[20px]"
+        laptop:text-[20px] laptop:pb-[20px]"
         >
           {logo}
         </p>
