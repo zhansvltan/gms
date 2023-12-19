8b586db9c2c153273efe44bdd03cc2955a1fde51
zhansvltan <assemzhar@icloud.com>
2023-12-20 00:49:29 +0600

    Project section mobile adaptation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,41 @@ import React from 'react'
 const Card = ({ imageSrc, name, description, logo }) => {
   return (
     <div
-      style={{ height: '431px' }}
-      className="rounded-[24px] overflow-hidden shadow-[0px_2px_10px_rgba(3,3,3,0.1)]"
+      className="rounded-[24px] overflow-hidden shadow-[0px_2px_10px_rgba(3,3,3,0.1)]
+      max-h-[481px]
+      mobile:h-[60%]
+      tablet:h-[75%] 
+      laptop: h-[100%]"
     >
-      <img className="w-full pb-[25px]" src={imageSrc} alt={name} />
-      <div className="pl-[25px]">
-        <h3 className="text-[24px] text-[#030303] pb-[20px]">{name}</h3>
-        <p style={{ height: '144px' }} className="text-[#030303] text-[20px]">
+      <img
+        className="w-full mobile:pb-[15px] tablet:pb-[20px] laptop:pb-[25px]"
+        src={imageSrc}
+        alt={name}
+      />
+      <div className="mobile:pl-[15px] tablet:pl-[20px] laptop:pl-[25px]">
+        <h3
+          className="text-[#030303] mobile:text-[16px] mobile:pb-[12px] 
+        tablet:text-[20px] tablet:pb-[16px] 
+        laptop:text-[24px] laptop: pb-[20px]"
+        >
+          {name}
+        </h3>
+        <p
+          className="text-[#030303] 
+        mobile:h-[96px]  mobile:text-[12px] 
+        tablet:h-[120px] tablet:text-[16px] 
+        laptop:h-[144px] laptop:text-[20px]"
+        >
           {description}
         </p>
-        <p className="text-[#030303] text-[20px] pb-[20px]">{logo}</p>
+        <p
+          className="text-[#030303]  
+        mobile:text-[12px] mobile:pb-[12px] 
+        tablet:text-[16px] tablet:pb-[16px]  
+        laptop:text-[20px] laptop: pb-[20px]"
+        >
+          {logo}
+        </p>
       </div>
     </div>
   )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,42 +1,50 @@
-import React from "react";
-import Card from "./Card";
+import React from 'react'
+import Card from './Card'
 
 const ProjectComponent = () => {
   const cardsData = [
     {
-      imageSrc: "project1.svg",
-      name: "Проект №1",
-      description: "Описание проекта №1",
-      logo: "Logo",
+      imageSrc: 'project1.svg',
+      name: 'Проект №1',
+      description: 'Описание проекта №1',
+      logo: 'Logo',
     },
     {
-      imageSrc: "project2.svg",
-      name: "Проект №2",
-      description: "Описание проекта №2",
-      logo: "Logo",
+      imageSrc: 'project2.svg',
+      name: 'Проект №2',
+      description: 'Описание проекта №2',
+      logo: 'Logo',
     },
     {
-      imageSrc: "project3.svg",
-      name: "Проект №3",
-      description: "Описание проекта №3",
-      logo: "Logo",
+      imageSrc: 'project3.svg',
+      name: 'Проект №3',
+      description: 'Описание проекта №3',
+      logo: 'Logo',
     },
-  ];
+  ]
 
   return (
-    <div className="pb-[144px] bg-white ">
-      <div className="pl-[55px] pt-[42px]">
-        <h2 className="text-[42px] pb-[55px] font-[500] text-[#5d5d5b]">
+    <div className="mobile:pb-[96px] tablet:pb-[120px] laptop:pb-[144px] bg-white ">
+      <div
+        className="mobile:pl-[30px] mobile:pt-[20px]
+                tablet:pl-[45px] tablet:pt-[30px]
+                laptop:pl-[55px] laptop:pt-[38px] "
+      >
+        <h2
+          className="mobile:text-[24px] 
+                tablet:text-[36px] 
+                laptop:text-[42px] pb-[55px] font-[500] text-[#5d5d5b]"
+        >
           Наши реализованные проекты
         </h2>
       </div>
-      <div className="flex justify-around">
+      <div className="flex justify-around mobile:flex-col space-y-3 laptop:flex-row">
         {cardsData.map((card, index) => (
           <Card key={index} {...card} />
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProjectComponent;
+export default ProjectComponent
