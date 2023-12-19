55ebff849bcebba70961514cd5c9e8a353f94820
zhansvltan <assemzhar@icloud.com>
2023-12-20 01:05:33 +0600

    Partner section mobile adaptation

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react'
 
 export default function PartnerComponent() {
   return (
-    <div className="flex justify-center bg-white items-baseline">
+    <div className="flex justify-center bg-white items-baseline mobile:flex-col laptop:flex-row">
       <h2
-        style={{ maxWidth: "504px", width: "100%" }}
-        className="text-[#030303] text-[42px] font-[500]"
+        className="text-[#030303] font-[500]
+                mobile:text-[24px] w-[354px]
+                tablet:text-[36px] w-[404px]
+                laptop:text-[42px] w-[504px]"
       >
         Наши клиенты и партнеры
       </h2>
-      <div className="flex-col pb-[130px]">
+      <div className="flex-col mobile:pt-[15px] mobile:pb-[90px] laptop:pt-[0px] laptop:pb-[130px]">
         <div className="flex justify-between">
           <div>
             <img src="lancaster.svg" alt="Lancaster" />
@@ -18,7 +20,7 @@ export default function PartnerComponent() {
             <img src="world.svg" alt="World Bank Group" />
           </div>
         </div>
-        <div className="flex justify-between pt-[30px]">
+        <div className="flex justify-between mobile:pt-[20px] laptop:pt-[30px]">
           <div>
             <img src="aifc.svg" alt="AIFC" />
           </div>
@@ -28,5 +30,5 @@ export default function PartnerComponent() {
         </div>
       </div>
     </div>
-  );
+  )
 }
