28d94c05c1b414a77d6b8eec3b5b8fc33c72ea38
zhansvltan <assemzhar@icloud.com>
2023-12-21 17:14:05 +0600

    Group section modify

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 export default function GroupsComponent() {
   return (
-    <div
-      className="bg-white
-                laptop:pl-[55px] laptop:pt-[38px] laptop:pb-16"
-    >
+    <div className="bg-white">
       <h2
         className="font-[500] text-[#5d5d5b]
                   mobile:text-[24px]
                   tablet:text-[36px]
-                  laptop:text-[42px] laptop:pb-12"
+                  laptop:text-[42px]"
       >
         О группе компаний
       </h2>
